87c41073c58730d05bebed10a11c366fb907134b
Alex Romanov <alex@bimodalconsulting.com>
2022-08-02 21:22:06 -0700

    Fix IMG Alt attribute

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,5 @@
 import image from './image.jpg'
 
-export const Image = ({alt, role}: {alt?: string; role?: string}) => (
+export const Image = ({ alt = 'M&Ms-shaped various colours objects', role }: { alt?: string; role?: string }) => (
   <img src={image} role={role} alt={alt} width='600px' />
 )
